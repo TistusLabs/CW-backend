817c506fb1ca986fb08a7d7f10b6083bef09e078
Shehan Tis <shehanproductions@ymail.com>
2018-03-27 21:50:46 +0530

    updated user model

--- app/models/vehicle.js
+++ app/models/vehicle.js
@@ -11,8 +11,19 @@ var VehicleSchema = new Schema({
     make: String,
     model: String,
     currentMileage: Number,
-    taggedToCustomer:Boolean,
+    taggedToCustomer: Boolean,
+    profileID: String,
     create_date: Date,
 });
+
+VehicleSchema.methods.TagToCustomer = function (profileID) {
+    this.taggedToCustomer = true;
+    this.profileID = profileID;
+}
+
+VehicleSchema.methods.UntagCustomer = function (profileID) {
+    this.taggedToCustomer = false;
+    this.profileID = "";
+}
 
 module.exports = mongoose.model('Vehicle', VehicleSchema);
